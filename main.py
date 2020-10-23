328f107d82ae0fd0966baaec559159e3cd268193
Kyle Rhyner <klrhyner@u.washington.edu>
2020-10-23 16:59:49 -0700

    practice api consumption

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     #
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
+    response = requests.get(f'https://api.github.com/users/{username}/events')
+    #print(response)    ->  <Response [200]>
+    #need to access the repsonse.content using json
+    print(response.json()[0]['created_at'])
     
 
 
